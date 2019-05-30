d5c0a3cc396b60bc4e30c9423e67f72fdb7addcf
pancakeportal99 <doofevil10@gmail.com>
2019-05-30 00:24:12 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+function getWebsite(e) {
+    var t = new XMLHttpRequest;
+    return t.open("GET", e, !1), t.send(), t.responseText
+}
 client.on('ready', () => {
     console.log('I am ready!');
+    
 });
 
 
 client.on('message', message => {
     if (message.content === '!quote') {
-       message.channel.send('My Message');
+       var scooter = JSON.parse(getWebsite("https://howmuchisthe.fish/json/random"));
+       var content = "**\"" + scooter.quote.text + "\"** \n\t\t- H.P Baxxter, " + scooter.quote.track + " (" + scooter.quote.year + ")"
+       message.channel.send(content);
        }
 });
 
